dde8d40869f65016db7732af0f6c4b78f4dff292
Alifr23 <alifr22@yahoo.co.uk>
2024-11-30 17:07:21 +0000

    Adjust home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Card } from "./components/Card";
+import { Stack } from "./components/Stack";
 import { Typography } from "./components/Typography";
 
 export default function Home() {
   return (
-    <div>
+    <Stack direction="vertical" gap={20}>
       <Typography variant="h1">
         High-converting B2B websites that flood your business with ready-to-buy
         customers
@@ -11,16 +13,24 @@ export default function Home() {
         Design, copy, and development engineered to turn visitors into customers
         and drive sales on autopilot.
       </Typography>
-      <Typography variant="h3">Get a free proposal</Typography>
-      <Typography variant="h4">Get a free proposal</Typography>
-      <Typography variant="h5">Get a free proposal</Typography>
-      <Typography variant="h6">Get a free proposal</Typography>
-      <Typography variant="body1">Get a free proposal</Typography>
-      <Typography variant="body2">Get a free proposal</Typography>
-      <Typography variant="body3">Get a free proposal</Typography>
-      <Typography variant="body4">Get a free proposal</Typography>
-      <Typography variant="body5">Get a free proposal</Typography>
-      <Typography variant="caption">Get a free proposal</Typography>
-    </div>
+      <Card
+        theme="dark"
+        paddingY={32}
+        paddingX={24}
+        borderRadius={20}
+        borderless={false}
+        borderThickness={2}
+      >
+        <Stack direction="horizontal" gap={48}>
+          <Typography variant="body2">About Us</Typography>
+          <Typography variant="body2">Services</Typography>
+          <Typography variant="body2">Pricing</Typography>
+          <Typography variant="body2">Packages</Typography>
+          <Typography variant="body2" color="#FBFF4B">
+            Contact Us
+          </Typography>
+        </Stack>
+      </Card>
+    </Stack>
   );
 }
