4d0a5b7f879c05d9fc4535459dc4fcd9646b43f7
Alifr23 <alifr22@yahoo.co.uk>
2024-11-30 12:17:22 +0000

    Add dev mode page extensions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+const isProduction = process.env.NODE_ENV === "production";
+
+const devPageExtensions = ["wip.tsx", "wip.ts", "wip.js", "wip.jsx"];
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    "page.js",
+    "page.jsx",
+    ...(isProduction ? [] : devPageExtensions),
+  ],
 };
 
 export default nextConfig;
