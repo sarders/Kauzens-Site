72db41b1128ff9c5dd26310a0ac39a084c53321c
Alifr23 <alifr22@yahoo.co.uk>
2024-11-30 14:44:47 +0000

    Add inter tight as font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Inter_Tight } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const interTight = Inter_Tight({
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${interTight.className}`}>{children}</body>
     </html>
   );
 }
